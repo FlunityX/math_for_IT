5b35712d46f65d2426c5430af50e3107b5a25b91
FlunityX <149858650+FlunityX@users.noreply.github.com>
2023-12-05 23:44:10 +0700

    đạo hàm và lý thuyết đồ thị

---
+++ lý thuyết đồ thị/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace lý_thuyết_đồ_thị
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("TÌM HIỂU VỀ LÝ THUYẾT ĐỒ THỊ ");
+            Console.WriteLine("Dưới dây là 13 khái niệm về lý thuyết đồ thị , vui lòng chọn con số để xem lý thuyết : ");
+            Console.WriteLine("1.Đơn đồ thị vô hướng.");
+            Console.WriteLine("2.Đa đồ thị vô hướng ");
+            Console.WriteLine("3.Giả đồ thị vô hướng ");
+            Console.WriteLine("4.Đơn đồ thị có hướng.");
+            Console.WriteLine("5.Đa đồ thị có hướng ");
+            Console.WriteLine("6.Đỉnh kề , cạnh liên thuộc ");
+            Console.WriteLine("7.Bậc của đồ thị vô hướng ");
+            Console.WriteLine("8.Bán bậc vào , bán bậc ra ");
+            Console.WriteLine("9.Đường đi ");
+            Console.WriteLine("10.Chu trình ");
+            Console.WriteLine("11.Liên thông ");
+            Console.WriteLine("12.Thành phần liên thông ");
+            Console.WriteLine("13.Liên thông mạnh , liên thông yếu ");
+            Console.Write("Nhập con số muốn xem khái niệm ");
+            int output;
+            string n = Console.ReadLine();
+            if (int.TryParse(n,out output) && output >= 1 && output <= 13)
+            {
+                DisplayConcept(output);
+            }
+            else
+            {
+                Console.WriteLine("ko có số đó vui lòng nhập lại ");
+            }
+        }
+        static void DisplayConcept(int output)
+        {
+            switch (output)
+            {
+                case 1:
+                    Console.WriteLine("Đơn đồ thị vô hướng: Đồ thị không hướng, không có chiều.");
+                    break;
+                case 2:
+                    Console.WriteLine("Đa đồ thị vô hướng: Đồ thị có thể có nhiều cạnh giữa các đỉnh.");
+                    break;
+                case 3:
+                    Console.WriteLine("Giả đồ thị vô hướng: Đồ thị không có vòng (cycle), có khuyên ");
+                    break;
+                case 4:
+                    Console.WriteLine("Đơn đồ thị có hướng: Đồ thị có hướng từ một đỉnh đến một đỉnh khác.");
+                    break;
+                case 5:
+                    Console.WriteLine("Đa đồ thị có hướng: Đồ thị có thể có nhiều cạnh hướng giữa các đỉnh.");
+                    break;
+                case 6:
+                    Console.WriteLine("Đỉnh kề, cạnh liên thuộc: Đỉnh kề là các đỉnh được nối bằng cạnh.");
+                    break;
+                case 7:
+                    Console.WriteLine("Bậc của đồ thị vô hướng: Bậc của một đỉnh là số cạnh kề với nó.");
+                    break;
+                case 8:
+                    Console.WriteLine("Bán bậc vào, bán bậc ra: Bán bậc vào là số cạnh đi vào một đỉnh, bán bậc ra là số cạnh đi ra từ một đỉnh.");
+                    break;
+                case 9:
+                    Console.WriteLine("Đường đi: Dãy đỉnh liên tiếp nhau qua các cạnh, không qua đỉnh nào lặp lại.");
+                    break;
+                case 10:
+                    Console.WriteLine("Chu trình: Đường đi tạo thành một vòng, bắt đầu và kết thúc ở cùng một đỉnh.");
+                    break;
+                case 11:
+                    Console.WriteLine("Liên thông: Mọi cặp đỉnh đều có đường đi giữa chúng.");
+                    break;
+                case 12:
+                    Console.WriteLine("Thành phần liên thông: Một tập hợp các đỉnh mà giữa chúng có đường đi.");
+                    break;
+                case 13:
+                    Console.WriteLine("Liên thông mạnh, liên thông yếu: Liên thông mạnh đồng nghĩa với mỗi cặp đỉnh đều có đường đi cả hai chiều giữa chúng.");
+                    Console.WriteLine("Liên thông yếu đồng nghĩa với có ít nhất một chiều có đường đi giữa mỗi cặp đỉnh.");
+                    break;
+                default:
+                    Console.WriteLine("Không có khái niệm cho số này.");
+                    break;
+            }
+        }
+
+    }
+}
---
+++ đạo_hàm/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace đạo_hàm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  TINH DAO HAM   ");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("1. dao ham cua 1 hang so : y = (k.x)' :");
+            Console.WriteLine("2. dao ham y = (x^n)' ");
+            Console.WriteLine("3. dao ham y = (can x)' ");
+            Console.WriteLine("4. dao ham y = (1/x)'");
+            Console.WriteLine("nhap lua chon cua ban :");
+            if (int.TryParse(Console.ReadLine(), out int chon))
+            {
+                TinhDaoHam(chon);
+            }
+            else
+            {
+                Console.WriteLine("Nhap khong hop le. Hay nhap mot so nguyen.");
+            }
+            Console.ReadLine();
+        }
+        public static void TinhDaoHam(int chon)
+        {
+            switch (chon)
+            {
+                case 1:
+                    Console.WriteLine("dao ham la y = x");
+                    Console.WriteLine("nhap gia tri x : ");
+                    int a = int.Parse(Console.ReadLine());
+                    Console.WriteLine("gia tri y = {0} ", a);
+                    break;
+                case 2:
+                    Console.WriteLine("nhap co so (x) :");
+                    int b = int.Parse(Console.ReadLine());
+                    Console.WriteLine("nhap so mu (n) :");
+                    int c = int.Parse(Console.ReadLine());
+                    Console.WriteLine("dao ham la y = {0}.x^({1}) ", b, b - 1);
+                    double d = c * Math.Pow(b, c - 1);
+                    Console.WriteLine("gia tri y = {0}", d);
+                    break;
+                case 3:
+                    Console.WriteLine("dao ham la : y = 1/ 2 can x ");
+                    Console.WriteLine("nhap co so x :");
+                    int f = int.Parse(Console.ReadLine());
+                    double daoham4 = 1 / (double)2 * Math.Sqrt(f);
+                    Console.WriteLine(" gia tri y la : {0} ", daoham4);
+                    break;
+
+                case 4:
+                    Console.WriteLine("dao ham la y = -1/x^2 ");
+                    Console.WriteLine("nhap co so (x) :");
+                    int e = int.Parse(Console.ReadLine());
+                    double daoham3 = -1 / (double)Math.Pow(e, 2);
+                    Console.WriteLine("gia tri y = {0} ", daoham3);
+                    break;
+                
+                default:
+                    return;
+                    
+            }
+        }
+    }
+}
