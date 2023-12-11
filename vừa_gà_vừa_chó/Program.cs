13b49fd6e5341b5ecf3794068706a56e0c692ff2
FlunityX <149858650+FlunityX@users.noreply.github.com>
2023-12-11 18:53:41 +0700

    phương trình bậc nhất 2 ẩn

---
+++ vừa_gà_vừa_chó/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vừa_gà_vừa_chó
+{
+    /*Vừa gà vừa chó
+
+    Bó lại cho tròn
+
+    Ba mươi sáu con
+
+   Một trăm chân chẵn
+
+     Một câu đố dân gian nổi tiếng chắc hẳn tất cả các bạn đều biết.Và bạn có thể cũng đã biết đáp án của câu đố trên là
+    22 con gà và 14 con chó.Tuy nhiên, thầy giáo của Hiếu lại bắt anh ấy phải giải bài tập tổng quát: "Tìm số gà, số chó khi
+    số tổng số con là m và số chân là n
+    nếu m và n khiến số gà số chó ko phù hợp in -1
+. Bạn hãy giúp Hiếu giải bài toán này nhé.*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nhap tong so con");
+            int m=int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap so chan ");
+            int n =int.Parse(Console.ReadLine());
+            tinhsocon(n, m);
+            Console.ReadLine();
+        }
+        static void  tinhsocon(int m , int n)
+        {
+            int soga;
+            int socho;
+            // số gà chạy từ 0 đến m số chó = m - số gà , nếu thảo mãn thêm điều kiện  if (4 * socho + 2 * soga == n) thì in ra so gà chó
+            for (soga = 0; soga <= m; soga++)
+            {
+                socho = m - soga;
+                if (4 * socho + 2 * soga == n)
+                {
+                    Console.WriteLine("Số lượng gà: " + soga);
+                    Console.WriteLine("Số lượng chó: " + socho);
+                    return;
+                }
+            }
+            Console.WriteLine("-1");
+
+        }
+    }
+}
---
+++ vừa_gà_vừa_chó/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("vừa_gà_vừa_chó")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("vừa_gà_vừa_chó")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("64055aea-1cde-4322-8466-c6ad1783bf77")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
